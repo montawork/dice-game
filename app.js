baed6f2d1bd08bb6074f032b73db18c60c9f585f
montawork <hmonta.work@gmail.com>
2022-03-25 16:48:32 +0100

    code refactoring && sound fix

--- app.js
+++ app.js
@@ -15,8 +15,6 @@ const [
   holdBtn,
   scoreOne,
   scoreTwo,
-  currentOne,
-  currentTwo,
 ] = selectDOMElements([
   '.player--0',
   '.player--1',
@@ -26,16 +24,13 @@ const [
   '.btn--hold',
   '#score--0',
   '#score--1',
-  '#current--0',
-  '#current--1',
 ]);
 
 // global vars
 let currentScore = 0;
-let totalOne = 0;
-let totalTwo = 0;
-const winAudio = new Audio('./sounds/win.wav');
-const loseAudio = new Audio('./sounds/lose.wav');
+let scores = [0, 0];
+let activePlayer = 0;
+let isGameOver = false;
 // all dice
 const images = [
   './images/dice-1.png',
@@ -47,104 +42,73 @@ const images = [
 ];
 
 // events
-rollDiceBtn.addEventListener('mouseup', rollDice);
-holdBtn.addEventListener('mouseup', holdGame);
+rollDiceBtn.addEventListener('click', rollDice);
+holdBtn.addEventListener('click', holdGame);
 newGameBtn.addEventListener('click', resetGame);
+
+const changePlayer = () => {
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).innerText = 0;
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  playerOne.classList.toggle('player--active');
+  playerTwo.classList.toggle('player--active');
+};
 
 // play
 function rollDice() {
-  // display sound
-  const rollAudio = new Audio('./sounds/roll.mp3');
-  rollAudio.play();
-  if (
-    !playerOne.classList.contains('player--winner') &&
-    !playerTwo.classList.contains('player--winner')
-  ) {
+  if (!isGameOver) {
+    // display sound
+    new Audio('./sounds/roll.mp3').play();
+    // current values
+    const randomDice = Math.floor(Math.random() * images.length);
+    const currentImg = images[randomDice];
+    diceImage.src = currentImg;
     diceImage.style.display = 'block';
-  }
-  // current values
-  const randomDice = Math.floor(Math.random() * images.length);
-  const currentImg = images[randomDice];
-  diceImage.src = currentImg;
-  const currentDice = +currentImg.match(/\d/g)[0];
-  currentScore += currentDice;
-  // player 1
-  if (playerOne.classList.contains('player--active')) {
+    const currentDice = +currentImg.match(/\d/g)[0];
+    currentScore += currentDice;
+    // change player
     if (currentDice === 1) {
-      // display sound
-      loseAudio.play();
-      currentScore = 0;
-      currentOne.innerText = currentScore;
-      playerOne.classList.remove('player--active');
-      playerTwo.classList.add('player--active');
+      new Audio('./sounds/lose.wav').play();
+      changePlayer();
     } else {
-      currentOne.innerText = currentScore;
-    }
-  }
-  // player 2
-  else if (playerTwo.classList.contains('player--active')) {
-    if (currentDice === 1) {
-      // display sound
-      loseAudio.play();
-      currentScore = 0;
-      currentTwo.innerText = currentScore;
-      playerTwo.classList.remove('player--active');
-      playerOne.classList.add('player--active');
-    } else {
-      currentTwo.innerText = currentScore;
+      document.getElementById(`current--${activePlayer}`).innerText =
+        currentScore;
     }
   }
 }
 
 function holdGame() {
-  // sound
-  const audio = new Audio('./sounds/next.wav');
-  audio.play();
-  if (playerOne.classList.contains('player--active')) {
-    if (totalOne + currentScore < 100) {
-      totalOne += currentScore;
-      scoreOne.innerText = totalOne;
-      currentOne.innerText = 0;
-      playerOne.classList.remove('player--active');
-      playerTwo.classList.add('player--active');
-      currentScore = 0;
+  if (!isGameOver) {
+    // sound
+    new Audio('./sounds/next.wav').play();
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).innerText =
+      scores[activePlayer];
+    if (scores[activePlayer] < 100) {
+      changePlayer();
     } else {
-      winAudio.play();
-      scoreOne.innerText = totalOne + currentScore;
-      playerOne.classList.remove('player--active');
-      playerOne.classList.add('player--winner');
-      diceImage.style.display = 'none';
-    }
-  } else if (playerTwo.classList.contains('player--active')) {
-    if (totalTwo + currentScore < 100) {
-      totalTwo += currentScore;
-      scoreTwo.innerText = totalTwo;
-      currentTwo.innerText = 0;
-      playerTwo.classList.remove('player--active');
-      playerOne.classList.add('player--active');
-      currentScore = 0;
-    } else {
-      winAudio.play();
-      scoreTwo.innerText = totalTwo + currentScore;
-      playerTwo.classList.remove('player--active');
-      playerTwo.classList.add('player--winner');
+      new Audio('./sounds/win.wav').play();
+      isGameOver = true;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
       diceImage.style.display = 'none';
     }
   }
 }
 // reset
 function resetGame() {
-  const startAudio = new Audio('./sounds/start.wav');
-  startAudio.play();
-  totalOne = 0;
-  scoreOne.innerText = 0;
-  totalTwo = 0;
-  scoreTwo.innerText = 0;
+  new Audio('./sounds/start.wav').play();
+  isGameOver = false;
   currentScore = 0;
-  currentOne.innerText = 0;
-  currentTwo.innerText = 0;
-  playerTwo.classList.remove('player--active');
-  playerTwo.classList.remove('player--winner');
-  playerOne.classList.remove('player--winner');
+  scores = [0, 0];
+  scoreOne.innerText = 0;
+  scoreTwo.innerText = 0;
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
   playerOne.classList.add('player--active');
 }
